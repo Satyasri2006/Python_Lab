ebea6a2c6cbab61ecfa43706c79457ce2ad5d786
CHIKKARAJU NAGA RATNA SATYASRI <satyachikkaraju2016@gmail.com>
2025-11-08 12:02:08 +0530

    Update 1.5 - Fibonacci function.py

--- 1.5 - Fibonacci function.py
+++ 1.5 - Fibonacci function.py
@@ -1,14 +1,17 @@
 def fib(n) :
     a=0
     b=1
-    if (n==0) :
+    if(n<0) :
+        print("Invalid input")
+    elif n==0 :
         print(a)
-    if (n==1) :
+    elif n==1 :
         print(b)
     else :
-        c=a+b
-        print(c)
-        a=b
-        b=c
+        for i in range(2, n) :
+            c=a+b
+            a=b
+            b=c
+        return b
 num=int(input("Enter a number: "))
-fib(num)
+print("The ", n, "th fibonacci number is ", fib(n))
